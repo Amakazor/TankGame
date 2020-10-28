9cea45da538e7648a8d19b72afe3ddbb519dbf3c
amakazor <amakazor@gmail.com>
2020-10-28 19:26:15 +0100

    Basic rendering interfaces setup

---
+++ TankGame/Src/Actors/IRenderable.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using TankGame.Src.Gui.RenderComponents;
+
+namespace TankGame.Src.Actors
+{
+    public interface IRenderable
+    {
+        List<IRenderComponent> GetRenderObjects();
+    }
+}
---
+++ TankGame/Src/Gui/RenderComponents/IRenderComponent.cs
@@ -0,0 +1,11 @@
+﻿using SFML.Graphics;
+using TankGame.Src.Actors;
+
+namespace TankGame.Src.Gui.RenderComponents
+{
+    public interface IRenderComponent
+    {
+        public IRenderable GetActor();
+        public Transformable GetShape();
+    }
+}
