b8d4de1ee85ba6ee7c8824eec59af41294e17b2f
amakazor <amakazor@gmail.com>
2020-10-28 19:35:48 +0100

    basic Gamestate setup

---
+++ TankGame/Src/Data/GamestateManager.cs
@@ -0,0 +1,21 @@
+﻿namespace TankGame.Src.Data
+{
+    class GamestateManager
+    {
+        private static GamestateManager instance;
+
+        public long Points { get; private set; }
+
+        private GamestateManager()
+        {
+            Points = 0;
+        }
+
+        public static GamestateManager Instance { get { return instance ?? (instance = new GamestateManager()); } }
+
+        public void AddPoints(long points)
+        {
+            Points += points;
+        }
+    }
+}
