2d901584297376e10133a55afa1e4ebf6a3836fa
amakazor <amakazor@gmail.com>
2020-11-04 19:27:03 +0100

    Created Field Class

---
+++ TankGame/Src/Actors/Fields/Field.cs
@@ -0,0 +1,31 @@
+﻿using SFML.Graphics;
+using SFML.System;
+using System;
+using System.Collections.Generic;
+using TankGame.Src.Gui.RenderComponents;
+
+namespace TankGame.Src.Actors.Fields
+{
+    internal class Field : Actor
+    {
+        public Vector2i Coords { get; }
+        public float SpeedModifier { get; }
+        public bool IsTraversible { get; }
+
+        private SpriteComponent Surface { get; }
+
+        public Field(Vector2i coords, float speedModifier, bool isTraversible, Texture texture) : base(new Vector2f(coords.X * 64, coords.Y * 64), new Vector2f(64, 64))
+        {
+            Coords = coords;
+            SpeedModifier = speedModifier;
+            IsTraversible = isTraversible;
+
+            Surface = new SpriteComponent(Position, Size, this, texture, new Color());
+        }
+
+        public override HashSet<IRenderComponent> GetRenderComponents()
+        {
+            return new HashSet<IRenderComponent> { Surface };
+        }
+    }
+}
