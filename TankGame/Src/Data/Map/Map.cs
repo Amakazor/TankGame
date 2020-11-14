08789fd20bb75711050c9b5c26214085318da1e9
amakazor <amakazor@gmail.com>
2020-11-14 10:57:28 +0100

    Map  part 1

---
+++ TankGame/Src/Data/Map/Map.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TankGame.Src.Data.Map
+{
+    class Map
+    {
+        private HashSet<Region> Regions { get; }
+
+        public Map()
+        {
+
+        }
+    }
+}
