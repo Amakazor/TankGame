49a9cbe249e9b66b25ef6cf6e8bff85cb422f0f2
amakazor <amakazor@gmail.com>
2020-11-24 15:56:09 +0100

    implemented TickableActor

---
+++ TankGame/Src/Actors/TickableActor.cs
@@ -0,0 +1,25 @@
+﻿using SFML.System;
+using System;
+using TankGame.Src.Events;
+
+namespace TankGame.Src.Actors
+{
+    internal abstract class TickableActor : Actor, ITickable
+    {
+        public TickableActor(Vector2f position, Vector2f size) : base(position, size)
+        {
+        }
+
+        public void RegisterTickable()
+        {
+            MessageBus.Instance.PostEvent(MessageType.RegisterTickable, this, new EventArgs());
+        }
+
+        public void UnregisterTickable()
+        {
+            MessageBus.Instance.PostEvent(MessageType.UnregisterTickable, this, new EventArgs());
+        }
+
+        public abstract void Tick(float deltaTime);
+    }
+}
