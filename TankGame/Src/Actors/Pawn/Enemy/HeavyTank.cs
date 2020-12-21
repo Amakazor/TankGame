cda8fb6a09fa29ce489212c899744b6bad8b9442
amakazor <amakazor@gmail.com>
2020-12-21 21:14:13 +0100

    Changed textures of enemies

--- TankGame/Src/Actors/Pawn/Enemy/HeavyTank.cs
+++ TankGame/Src/Actors/Pawn/Enemy/HeavyTank.cs
@@ -6,7 +6,7 @@ namespace TankGame.Src.Actors.Pawn.Enemy
 {
     internal class HeavyTank : Enemy
     {
-        public HeavyTank(Vector2f position, Vector2f size, AIMovementController aIMovementController) : base(position, size, TextureManager.Instance.GetTexture(TextureType.Pawn, "enemy1"))
+        public HeavyTank(Vector2f position, Vector2f size, AIMovementController aIMovementController) : base(position, size, TextureManager.Instance.GetTexture(TextureType.Pawn, "enemy3"))
         {
             MovementController = aIMovementController;
             SetHealth(1);
--- TankGame/Src/Actors/Pawn/Enemy/MediumTank.cs
+++ TankGame/Src/Actors/Pawn/Enemy/MediumTank.cs
@@ -6,7 +6,7 @@ namespace TankGame.Src.Actors.Pawn.Enemy
 {
     internal class MediumTank : Enemy
     {
-        public MediumTank(Vector2f position, Vector2f size, AIMovementController aIMovementController) : base(position, size, TextureManager.Instance.GetTexture(TextureType.Pawn, "enemy1"))
+        public MediumTank(Vector2f position, Vector2f size, AIMovementController aIMovementController) : base(position, size, TextureManager.Instance.GetTexture(TextureType.Pawn, "enemy2"))
         {
             MovementController = aIMovementController;
             SetHealth(1);
