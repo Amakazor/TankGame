a5d53533608d288368ce477012b89542fd03a7bf
amakazor <amakazor@gmail.com>
2020-12-21 21:11:42 +0100

    Added Medium Tank

---
+++ TankGame/Src/Actors/Pawn/Enemy/MediumTank.cs
@@ -0,0 +1,15 @@
+﻿using SFML.System;
+using TankGame.Src.Actors.Pawn.MovementControllers;
+using TankGame.Src.Data;
+
+namespace TankGame.Src.Actors.Pawn.Enemy
+{
+    internal class MediumTank : Enemy
+    {
+        public MediumTank(Vector2f position, Vector2f size, AIMovementController aIMovementController) : base(position, size, TextureManager.Instance.GetTexture(TextureType.Pawn, "enemy1"))
+        {
+            MovementController = aIMovementController;
+            SetHealth(1);
+        }
+    }
+}
