4afd0da8073d2314f76e6d5ac13921277b717063
amakazor <amakazor@gmail.com>
2021-01-10 13:32:39 +0100

    additional roads

--- TankGame/Src/Actors/GameObjects/GameObjectType.cs
+++ TankGame/Src/Actors/GameObjects/GameObjectType.cs
@@ -9,10 +9,10 @@ namespace TankGame.Src.Actors.GameObjects
         public static readonly Tuple<TraversibilityData, DestructabilityData, string> House = new Tuple<TraversibilityData, DestructabilityData, string> (new TraversibilityData(0F, false), new DestructabilityData(4, true, false), "gruz");
         public static readonly Tuple<TraversibilityData, DestructabilityData, string> Fence1 = new Tuple<TraversibilityData, DestructabilityData, string> (new TraversibilityData(1.5F, true), new DestructabilityData(2, true, true), "gruz");
         public static readonly Tuple<TraversibilityData, DestructabilityData, string> Fence2 = new Tuple<TraversibilityData, DestructabilityData, string> (new TraversibilityData(1.5F, true), new DestructabilityData(2, true, true), "gruz");
-        public static readonly Tuple<TraversibilityData, DestructabilityData, string> Gruz = new Tuple<TraversibilityData, DestructabilityData, string> (new TraversibilityData(1.25F, true), new DestructabilityData(1, false, false), null);
+        public static readonly Tuple<TraversibilityData, DestructabilityData, string> Gruz = new Tuple<TraversibilityData, DestructabilityData, string> (new TraversibilityData(1.25F, true), new DestructabilityData(1, false, false, false), null);
         public static readonly Tuple<TraversibilityData, DestructabilityData, string> Tree = new Tuple<TraversibilityData, DestructabilityData, string> (new TraversibilityData(2F, true), new DestructabilityData(1, true, true), "pien");
         public static readonly Tuple<TraversibilityData, DestructabilityData, string> Tree2 = new Tuple<TraversibilityData, DestructabilityData, string> (new TraversibilityData(2F, true), new DestructabilityData(1, true, true), "pien");
-        public static readonly Tuple<TraversibilityData, DestructabilityData, string> Pien = new Tuple<TraversibilityData, DestructabilityData, string> (new TraversibilityData(1.25F, true), new DestructabilityData(1, false, false), null);
+        public static readonly Tuple<TraversibilityData, DestructabilityData, string> Pien = new Tuple<TraversibilityData, DestructabilityData, string> (new TraversibilityData(1.25F, true), new DestructabilityData(1, false, false, false), null);
         
         public static readonly Dictionary<string, Tuple<TraversibilityData, DestructabilityData, string>> GameObjectTypes = new Dictionary<string, Tuple<TraversibilityData, DestructabilityData, string>>
         {
